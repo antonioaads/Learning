2ebdca7988d72c51aa29f9341ad0852a1361409c
Antônio Sousa <antonioaads@gmail.com>
2020-05-29 08:29:17 -0300

    Finalização do módulo de mobile do nivel 1

--- bootcamp/nivel1/mobile/src/index.js
+++ bootcamp/nivel1/mobile/src/index.js
@@ -1,28 +1,71 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#7159c1',
-    },
-    title: {
-        color: '#FFF',
-        fontSize: 30,
-        fontWeight: 'bold',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+  project: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 });
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    })
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Antônio Sousa'
+    });
+
+    setProjects([...projects, response.data]);
+  };
+
   return (
     <>
-        <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-        
-        <View style={styles.container} >
-            <Text style={styles.title}>Hello World</Text>
-        </View>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      
+      <SafeAreaView style={styles.container}>
+        <FlatList
+            data={projects}
+            keyExtractor={project => project.id}
+            renderItem={({ item }) => (
+                <Text style={styles.project}>{item.title}</Text>
+            )}
+        />
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </>
   )
 }
